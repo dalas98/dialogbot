81bb32fa5a4a6a912ac1221ba5862015cbd37ff9
Hans <mail@hans.com>
2021-06-12 13:34:20 +0700

    change stt to id

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 try {
   var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
   var recognition = new SpeechRecognition();
+  recognition.lang = 'id-ID'
 }
 catch(e) {
   console.error(e);
